116f9db69dd67a495e7b863f3ea53426c6929a13
THONV <tho.nv@svtech.com.vn>
2025-12-16 22:18:09 +0700

    them mói user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    // constructor(private readonly appService: AppService) {}
+
+    @Get()
+    getHello(): string {
+      return "Nguyễn Văn Thơ ";
+    }
+}
