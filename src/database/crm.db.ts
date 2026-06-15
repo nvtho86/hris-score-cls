6e0419dde27c4a8b7d1dc80c94790debc6d7e2ea
THONV <tho.nv@svtech.com.vn>
2026-06-15 20:32:00 +0700

    env

--- src/database/crm.db.ts
+++ src/database/crm.db.ts
@@ -1,11 +1,17 @@
 import { Pool } from 'pg';
 
 export const crmDb = new Pool({
-  host: '192.168.37.11',
-  port: 30100,
-  user: 'postgres',
-  password: 'postgres',
-  database: 'crm_score',
+  // host: '192.168.37.11',
+  // port: 30100,
+  // user: 'postgres',
+  // password: 'postgres',
+  // database: 'crm_score',
+  host: process.env.DB_HOST_SCORE,
+  port: process.env.DB_PORT_SCORE,
+  user: process.env.DB_USER_SCORE,
+  password: process.env.DB_PASS_SCORE,
+  database: process.env.DB_NAME_SCORE,
+  
 });
 
 
